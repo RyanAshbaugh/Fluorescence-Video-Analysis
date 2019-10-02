f93f3396726848b41b1bf43a1a7ae78ba9ae6c64
Ryan Ashbaugh <ashbau12@msu.edu>
2019-10-01 22:05:24 -0400

    Added file for setting meta data

--- setFileMetaData.m
+++ setFileMetaData.m
@@ -5,21 +5,18 @@ function meta = setFileMetaData(meta,cur_file)
 %		cur_file: string of files name
 %	Returns: meta- updated meta data struct
     
-    %undersc = strfind(meta.fpath,'_');           % get date of file
-    
     backslash = strfind(meta.dname,'\');         % find all '\' in name
 	meta.experiment_name = ...
 		meta.dname( backslash(end)+1:end )
     meta.date = meta.dname(backslash(end-1)+1:end-1); % go to last '\'
     
     % results folder and output filename
-    meta.results_folder = strcat('../Results/seizure-detection/',meta.date,...
+    meta.results_folder = strcat('../Results/fluorescence-video-analysis/',meta.date,...
 		'/',meta.experiment_name,'/');
 	meta.fname = cur_file.name;
     meta.fpath = [ meta.dname filesep meta.fname ];
 
-	meta.log_fpath = strcat(meta.results_folder,strrep(cur_file.name,'.smr','.log'));
-	meta.log_fpath = strcat(meta.results_folder,strrep(cur_file.name,'.smrx','.log'));
+	meta.log_fpath = strcat(meta.results_folder,strrep(cur_file.name,'.avi',''));
 
 	% create results folder if it doesn't exist
 	%full_results_folder = strcat(meta.results_folder,
