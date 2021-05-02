bccf08e4b15e3e274aa3e20409b165b49eda642f
Ryan Ashbaugh <ashbau12@msu.edu>
2021-05-01 21:02:54 -0400

    do full sequence calculation with all frames at once, without extra frame function and for loop

--- calculateROIPixelMeansSequence.m
+++ calculateROIPixelMeansSequence.m
@@ -1,13 +1,16 @@
 function sequence_roi_means = ...
-		calculateROIPixelMeansSequence( image_sequence, roi_mask )
+		calculateROIPixelMeansSequence( image_sequence, centroids )
 
-	num_frames = size( image_sequence,3 );
+	num_rois = size( centroids, 1 );
 	sequence_roi_means = zeros( size( roi_mask, 3 ), num_frames );
 	
-	for ii = 1:num_frames
+	for ii = 1:num_rois
+
+		x_center = round( centroids(ii, 1 ) );
+		y_center = round( centroids(ii, 2 ) );
 	
-		sequence_roi_means(:,ii) = ...
-			calculateROIPixelMeans( image_sequence(:,:,ii), roi_mask );
+		sequence_roi_means( ii, : ) = squeeze( mean( mean( ...
+			image_sequence(y_center-1:y_center+1,x_center-1:x_center+1,:))));
 
 	end
 end
