dcee733bb62f43f293c8e01b45b0f4b2cfa32cc2
Ryan Ashbaugh <ashbau12@msu.edu>
2021-05-02 12:51:10 -0400

    normalize to specific input baseline

--- normalizeTraces.m
+++ normalizeTraces.m
@@ -1,7 +1,7 @@
-function normalized_traces = normalizeTraces( input_traces )
+function normalized_traces = normalizeTraces( input_traces, base_index )
 
 	num_frames = size( input_traces, 2 );
-	normalizing_matrix = repmat( input_traces(:,1), 1, num_frames );;
+	normalizing_matrix = repmat( input_traces(:,base_index), 1, num_frames );
 	normalized_traces = ( input_traces ./ normalizing_matrix ) - 1;
 
 end
