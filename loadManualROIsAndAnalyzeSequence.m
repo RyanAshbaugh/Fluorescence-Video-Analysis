66ec3e65bd6a4da86068864a5e5edef53e100a38
Ryan Ashbaugh <ashbau12@msu.edu>
2021-06-12 13:40:16 -0700

    complete file for doing manual processing. select save file for all info csv and summary file

--- loadManualROIsAndAnalyzeSequence.m
+++ loadManualROIsAndAnalyzeSequence.m
@@ -22,18 +22,18 @@ median_filtered_sequence = medianFilterImageSequence( image_sequence, ...
 % create a mask for pixels of each label, image_height x width x num labels 
 ROI_mask = createManualROIMask( pixel_roi_image );
 
-
-%{
 % calculate the ROI means using the mask of per label pixel ROIs
-sequence_roi_means = calculateROIPixelMeansSequence( ...
-	median_filtered_sequence, cell_roi_centroids );
+sequence_roi_means = calculateManualROIMeanSequence( ...
+	median_filtered_sequence, ROI_mask );
 
 smoothed_roi_means = movmean( sequence_roi_means, moving_avg_width, 2 );
 
 plotROITraces( smoothed_roi_means );
 [ baseline_index, post_stim_index ] = getUserFrameNumberInput();
 
+% normalize the traces
 normalized_roi_means = normalizeTraces( smoothed_roi_means, baseline_index );
+
 delta_r = calculateDeltaBright( normalized_roi_means, post_stim_index );
 
 conditon_mean_delta_r = mean( delta_r ) * 100;
@@ -45,4 +45,19 @@ disp( [ 'Condition mean delta_R:   ' ...
 	num2str( conditon_mean_delta_r ) ' %' ] );
 disp( [ 'Condition median delta_R: ' ...
 	num2str( conditon_median_delta_r ) ' %' newline ] );
-%}
+
+% ask user to select file for saving normalized traces and summary
+[ roi_mean_filename, roi_mean_filepath ] = ...
+	uiputfile( '*.csv', 'Normalized ROI mean csv file' );
+num_frames = size( image_sequence, 3 );
+num_ROIs = size( ROI_mask, 3 );
+summary_row_names = { 'Number of frames', 'Number of ROIs', 'Baseline frame',...
+	'Post stim frame', 'Mean delta R (%)', 'Median delta R (%)' };
+summary_data = [ num_frames, num_ROIs, baseline_index, post_stim_index, ...
+	conditon_mean_delta_r, conditon_median_delta_r ];
+summary_table = table( summary_row_names, summary_data );
+
+summary_table_filename = sttrep( roi_mean_filename, '.csv', '_summary.csv' );
+writetable( summary_table, ...
+	fullfile( roi_mean_filepath, summary_table_filename ), ...
+	'WriteVariableNames', false );
